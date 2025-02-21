f4cbafc1df8d51a73e8dc8600c7bc4286fb659f2
itsdeepakydv <114461345+itsdeepakydv@users.noreply.github.com>
2025-02-21 16:09:04 +0530

    Add files via upload

---
+++ project/project-root/server/config/twitterConfig.js
@@ -0,0 +1,10 @@
+const { TwitterApi } = require('twitter-api-v2');
+
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_APP_KEY,
+  appSecret: process.env.TWITTER_APP_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET
+});
+
+module.exports = twitterClient;
